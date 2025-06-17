8faf575c10d95c3c7c55679ceb994fdc649e3300
Nameless <nuslans958@gmail.com>
2025-06-17 10:56:02 +0600

    wth

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from google import genai
 import json
 
 app = Flask(__name__)
+request.headers.get("User-Agent")
 
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY") or "AIzaSyCAbZBgv8pzC7o-m0SoPlQerQvlQwZPH68"
 client = genai.Client(api_key=GEMINI_API_KEY)
