3462ddffe617a2563c002ad3568481448e47d54b
Nameless <nuslans958@gmail.com>
2025-06-20 22:24:31 +0600

    small fix

--- main_esp32/main_esp32.ino
+++ main_esp32/main_esp32.ino
@@ -1,15 +1,9 @@
 #include <WiFi.h>
 #include <HTTPClient.h>
 #include <ArduinoJson.h>
-#include <ESP32Servo.h>
 
-#define SEED_SERVO 4
-#define HEAD_SERVO 5
-#define SOIL_MOS_SERVO_BIG 6
-#define SOIL_MOS_SERVO_SMALL 7
-
-const char* ssid = "Proteek Mesh";
-const char* password = "passwordnaisorry";
+const char* ssid = "Wifi abar ki jinis";
+const char* password = "passdemunah!";
 
 const char* seed_sowing_system = "https://agri-bot-kwis.onrender.com/seed_sowing_system/button";
 const char* humidity_measuring_system = "https://agri-bot-kwis.onrender.com/humidity_measuring_system/button";
@@ -21,24 +15,13 @@ const char* msg_of_humidity_measuring_system;
 const char* msg_of_water_pump_system;
 const char* msg_of_soil_moisture_measuring_system;
 
-Servo seed_servo;
-Servo head_servo;
-Servo soil_mos_servo_big;
-Servo soil_mos_servo_small;
+const char* on = "on";
+const char* off = "off";
 
 void setup() {
   Serial.begin(115200);
 
-  seed_servo.attach(SEED_SERVO);
-  head_servo.attach(HEAD_SERVO);
-  soil_mos_servo_big.attach(SOIL_MOS_SERVO_BIG);
-  soil_mos_servo_small.attach(SOIL_MOS_SERVO_SMALL);
-
-  seed_servo.write(0);
-  head_servo.write(0);
-  soil_mos_servo_big.write(0);
-  soil_mos_servo_small.write(0);
-
+  
   WiFi.begin(ssid, password);
   Serial.print("Connecting to WiFi");
   while (WiFi.status() != WL_CONNECTED) {
@@ -46,12 +29,16 @@ void setup() {
     Serial.print(".");
   }
   Serial.println("\nConnected to WiFi");
+  Serial2.begin(9600, SERIAL_8N1, 16, 17);
 }
 
 void loop() {
+
+
+
   if (WiFi.status() == WL_CONNECTED) {
     HTTPClient http;
-
+  
     // seed_sowing_system
     http.begin(seed_sowing_system);
     int httpCode = http.GET();
@@ -131,10 +118,30 @@ void loop() {
       Serial.printf("HTTP Error code (soil moisture): %d\n", httpCode);
     }
     http.end();
+    Serial.println(msg_of_soil_moisture_measuring_system);
+    Serial.println(on);
+
+    if (strcmp(msg_of_soil_moisture_measuring_system, on) == 0) {
+    Serial.println("meaw");
+    Serial2.println("on");
+}else{
+      Serial.println("meawww");
+    Serial2.println("off");
+}
+   if(strcmp(msg_of_seed_sowing_system, on) == 0) {
+    Serial.println("meaw");
+    Serial2.println("on1");
+}else{
+    Serial.println("meawww");
+    Serial2.println("off1");
+}
+
 
   } else {
     Serial.println("WiFi disconnected!");
   }
 
-  delay(5000); // To avoid spamming requests too fast
+
+  
+  
 }
