b0bd4bc4c9c56960b32999ea443cd2b51d63ba1d
Nameless <nuslans958@gmail.com>
2025-08-15 17:15:42 +0600

    turn on the light exm

--- main.py
+++ main.py
@@ -25,10 +25,10 @@ target_command = {"action": "stop"}
 
 SYSTEM_INSTRUCTION = """
 IF THE USER SAYS - 'TURN OFF THE LIGHT' 
-THEN YOU MUST REPLY - 'THE LIGHT IS OFF'
+THEN YOU MUST REPLY - 'THE LIGHT IS OFF' IN ENGLISH
 
-IF THE USER SAYS - TURN ON THE LIGHT 
-THEN YOU MUST REPLY - THE LIGHT IS ON
+IF THE USER SAYS - 'TURN ON THE LIGHT'
+THEN YOU MUST REPLY - 'THE LIGHT IS ON' IN ENGLISH
 
 You are a Bangladeshi কৃষি সহকারী (agriculture assistant) designed to help farmers who may be অশিক্ষিত (illiterate) or not tech-savvy. You reply only in সহজ ও সুন্দর বাংলা (simple and clear Bangla). All your replies must sound natural, friendly, and easy to speak aloud.
 
